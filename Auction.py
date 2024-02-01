2ce3d6e4cf420b9c0da70095b895f0fd8f606d2b
THORPUNURI SRIRAM <124619947+thorpunurisriam@users.noreply.github.com>
2024-02-01 11:37:35 +0530

    Auction.py

---
+++ Auction.py
@@ -0,0 +1,25 @@
+import os
+def findwinner(bidderdetails):
+    highestbid=0
+    winner=" "
+    for bidder in bidderdetails:
+        biddingprice=bidderdetails[bidder]
+        if biddingprice > highestbid:
+            highestbid=biddingprice
+            winner=bidder
+            
+    print(bidderdetails)        
+    print(f"The Winner is {winner} with a bid price of {highestbid}")
+            
+bidderdata={}
+endofbidding=False
+while not endofbidding:
+    name=input("What is your name:")
+    price=int(input("What is your bid? :"))
+    bidderdata[name]=price
+    morebidders=input("Are there more bidders? Type 'Yes' or 'no' :").lower()
+    if morebidders=='no':
+        endofbidding=True
+        findwinner(bidderdata)
+    elif morebidders=='Yes':
+        os.system('cls')
